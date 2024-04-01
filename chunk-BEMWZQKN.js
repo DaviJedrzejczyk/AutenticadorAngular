6be6e3b2359d5e9f96385ce74c26beaf52f117fd
SprilerBr <Djedrzejczyk88@gmail.com>
2024-04-01 17:10:17 -0300

    Auto-generated commit

---
+++ chunk-BEMWZQKN.js
@@ -0,0 +1,1 @@
+import"./chunk-SQJMWBRH.js";var t=[{path:"",title:"Sign In page",loadComponent:()=>import("./chunk-WVGK5N54.js")}];export{t as signRoute};
---
+++ chunk-WTAXTQDI.js
@@ -0,0 +1,1 @@
+import"./chunk-SQJMWBRH.js";var o=[{path:"",title:"Home page",loadComponent:()=>import("./chunk-5YKNKH3X.js")}];export{o as adminRoute};
